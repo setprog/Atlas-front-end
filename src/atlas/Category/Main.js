e7df86b15a8cceace77b9598e085739c76a5e351
Your Name <dagimwondim25@gmail.com>
2023-10-13 14:25:39 +0300

    edited css

--- src/atlas/Category/Main.js
+++ src/atlas/Category/Main.js
@@ -5,7 +5,7 @@ import React from "react";
            <input id="search" type="text" placeholder='choose a catagory'   ></input> 
            <h3>Showing {props.quant} results for {props.categ}</h3>
            <div className="inp">
-            <input type="text" placeholder="sort by"></input>
+            <input type="text" id="cat" placeholder="sort by"></input>
             <input type="text" id="cat" placeholder="Choose category"></input>
            </div>
         </div>
